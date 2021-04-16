d01245782699fc147b537c3c65ec3155c90b886c
connerlunn <clunn2@unl.edu>
2021-04-15 19:48:20 -0500

    added a basic start screen

--- index.js
+++ index.js
@@ -34,50 +34,63 @@ var text;
 var previousX;
 var previousY;
 
+var gameHasStarted = false;
+
 function create ()
 {
     leftArrowKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     upArrowKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
     rightArrowKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
-
-    biker = this.physics.add.image(200, 200, 'biker');
-    biker.setCollideWorldBounds(true);
-
-    text = this.add.text(0, 0, 'Use Arrow Keys to Move', { font: '"Arial"' });
+    text = this.add.text(0, 0, 'Up Arrow to Accelerate.\nLeft and Right Arrows to turn. \nPress any arrow key to start. ', { font: '"Arial"' });
     graphics = this.add.graphics({ lineStyle: { width: 4, color: 0xaa00aa } });
 
-    previousX = biker.x;
-    previousY = biker.y;
+    var Physics = this.physics;
+
+    this.input.keyboard.on('keyup', function (event) {
+        if ( !gameHasStarted && (event.keyCode === 37 || event.keyCode === 38 || event.keyCode === 39 || event.keyCode === 40))
+        {
+            biker = Physics.add.image(200, 200, 'biker');
+            biker.setCollideWorldBounds(true);
+
+            previousX = biker.x;
+            previousY = biker.y;
+        
+            gameHasStarted = true;
+        }
+    });
+
 }
 
 
 function update()
 {
-    text.text = "Angle: " + Math.round(biker.angle) + "   // AngleRateOfChange: " + angleRateOfChange;
+    if (gameHasStarted) {
+        text.text = "Angle: " + Math.round(biker.angle) + "   // AngleRateOfChange: " + Math.round(angleRateOfChange);
 
-    if (upArrowKey.isDown){
-        biker.angle += angleRateOfChange;
+        if (upArrowKey.isDown){
+            biker.angle += angleRateOfChange;
 
-        biker.x += Math.sin(biker.angle*3.1415/180);
-        biker.y -= Math.cos(biker.angle*3.1415/180);
+            biker.x += Math.sin(biker.angle*3.1415/180);
+            biker.y -= Math.cos(biker.angle*3.1415/180);
 
-        var line = new Phaser.Geom.Line(previousX, previousY, biker.x, biker.y);
-        graphics.strokeLineShape(line);
+            var line = new Phaser.Geom.Line(previousX, previousY, biker.x, biker.y);
+            graphics.strokeLineShape(line);
 
-        previousX = biker.x;
-        previousY = biker.y;
-    }
-    if (leftArrowKey.isDown)
-    {
-        if (angleRateOfChange > -1.5) {
-            angleRateOfChange -= .05;
+            previousX = biker.x;
+            previousY = biker.y;
         }
-    }
-    if (rightArrowKey.isDown)
-    {
-        if (angleRateOfChange < 1.5) {
-            angleRateOfChange += .05;
+        if (leftArrowKey.isDown)
+        {
+            if (angleRateOfChange > -1.5) {
+                angleRateOfChange -= .05;
+            }
+        }
+        if (rightArrowKey.isDown)
+        {
+            if (angleRateOfChange < 1.5) {
+                angleRateOfChange += .05;
+            }
         }
     }
 }
